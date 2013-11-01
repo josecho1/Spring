043c09c3013c9ba487661083f0f78199cbbcb6f1
josecho1 <jlbvalsa@gmail.com>
2013-11-01 21:42:14 +0100

    Engadindo datos de autor en EmployeeController Navigation Controler

--- SpringDataMeu/src/main/java/com/sprdata/controller/NavigationController.java
+++ SpringDataMeu/src/main/java/com/sprdata/controller/NavigationController.java
@@ -24,7 +24,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ *
+ * @author J.L. Villaverde jlvbalsa@gmail.com
+ */
 @Controller
 public class NavigationController {
 
